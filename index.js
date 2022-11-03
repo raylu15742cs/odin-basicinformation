11a38a20eac6b9bc753ef1e6d14544c9cc82555a
raylu15742cs <raylucs15742@gmail.com>
2022-11-03 10:00:57 -0700

    remove index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-fs.readFile('pages/about.html', 'utf8', function (err, data) {
-  // Display the file content
-  console.log(data);
-});
-
-console.log('readFile called');
